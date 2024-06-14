1dc61d5263e4a100a5949a58ba63af0fde450e76
EMMANUEL MBEWE <BED/COM/39/19@COMLAB.COM>
2024-06-14 05:06:06 +0300

    A

--- routes/user.js
+++ routes/user.js
@@ -9,13 +9,6 @@ const router = express.Router();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/meep', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('MongoDB connected');
-    createSuperUser();
-}).catch(err => console.error('MongoDB connection error:', err));
 
 const createSuperUser = async () => {
     try {
